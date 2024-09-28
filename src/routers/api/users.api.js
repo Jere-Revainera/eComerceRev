543b2fef4ae900f882692fe2323a82068a907773
Jere-Revainera <156834990+Jere-Revainera@users.noreply.github.com>
2024-09-28 11:38:38 -0300

    Update users.api.js

--- src/routers/api/users.api.js
+++ src/routers/api/users.api.js
@@ -1,17 +1,1 @@
-import { Router } from "express";
-import { getAllUsers,
-    getUsers,
-    createUser,
-    updateUser,
-    destroyUser,
-} from "../../controllers/users.controller.js"
 
-const usersApiRouter = Router()
-
-usersApiRouter.get("/", getAllUsers);
-usersApiRouter.get("/:uid", getUsers);
-usersApiRouter.post("/:name/:surname/:mail/", createUser)
-usersApiRouter.put("/:uid",updateUser)
-usersApiRouter.delete("/:uid", destroyUser)
-
-export default usersApiRouter
