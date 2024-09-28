990aeea1323a154d381809cc75c5d4029a4cfb85
Jere-Revainera <156834990+Jere-Revainera@users.noreply.github.com>
2024-09-28 11:38:16 -0300

    Update isValidData.mid.js

--- src/middlewares/isValidData.mid.js
+++ src/middlewares/isValidData.mid.js
@@ -1,16 +1,1 @@
-function isValidData(req, res, next) {
-  try {
-    const { title, stock, price } = req.body;
-    if (!title || !stock || !price) {
-      const error = new Error("Title, stock and price are required");
-      error.statusCode = 400;
-      throw error;
-    } else {
-      return next();
-    }
-  } catch (error) {
-    throw error;
-  }
-}
- 
-export default isValidData
+
