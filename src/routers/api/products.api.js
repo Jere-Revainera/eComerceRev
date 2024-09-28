b9cc0e1b2fce373d311b628fa00412e99e531c89
Jere-Revainera <156834990+Jere-Revainera@users.noreply.github.com>
2024-09-28 11:38:56 -0300

    Update products.api.js

--- src/routers/api/products.api.js
+++ src/routers/api/products.api.js
@@ -1,19 +1,1 @@
-import { Router } from "express";
-import {
-  getAllProducts,
-  getProducts,
-  createProduct,
-  updateProduct,
-  destroyProduct,
-} from "../../controllers/products.controller.js";
-import isValidData from "../../middlewares/isValidData.mid.js";
 
-const productsApiRouter = Router()
-
-productsApiRouter.get("/", getAllProducts);
-productsApiRouter.get("/:pid", getProducts);
-productsApiRouter.post("/", isValidData, createProduct);
-productsApiRouter.put("/:pid", updateProduct)
-productsApiRouter.delete("/:pid", destroyProduct)
-
-export default productsApiRouter
